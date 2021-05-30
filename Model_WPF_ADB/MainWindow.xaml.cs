86643be6863c780a77b3b07915959019f405e52c
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2021-05-30 14:48:30 +0700

    Commit duy nhất
    Đây là toàn bộ file khung sườn cho điều khiển điện thoại PC bằng WPF

---
+++ Model_WPF_ADB/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using KAutoHelper;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+
+namespace Model_WPF_ADB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
